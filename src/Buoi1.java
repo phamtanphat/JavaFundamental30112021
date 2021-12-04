3c68309e9a132343eb2c8573594826bab2dc4c52
phamtanphat <phatdroid94@gmail.com>
2021-12-04 19:33:37 +0700

    toan tu

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -1,28 +1,86 @@
 public class Buoi1 {
     // tổ hợp phím
     // gợi ý code : ctrl + space
+    // không muốn chạy đoạn lệnh (comment code) :  trỏ tới dòng không muốn chạy ctrl + /
     public static void main(String[] args) {
 
-        // 1 : Khai báo biến (variable) và kiểu dữ liệu (type)
+//         1 : Khai báo biến (variable) và kiểu dữ liệu (type)
+//
+//         Kiểu dữ liệu nguyên thủy : String, int , long , float , double , boolean , char...
+//         Kiễu dự liệu vùng nhớ (Object) : Object , Array;
+//
+//        String name = "Nguyễn Văn Tý";
+//        char text = 'a';
+//        int age = 12;
+//        float height = 1.2f;
+//        double weight = 35.5;
+//        boolean isMale = true;
+//
+//         build in function (phương thức được xây dựng sẵn)
+//         Xử lý chuỗi : "abc " + saewqe ( + chỉ dùng nhưu phép nối khi xuất hiện giá trị chuỗi)
+//        System.out.println("Name : " + name);
+//        System.out.println("Char : " + text);
+//        System.out.println("Age : " + age);
+//        System.out.println("Height : " + height);
+//        System.out.println("Weight : " + weight);
+//        System.out.println("isMale : " + isMale);
 
-        // Kiểu dữ liệu nguyên thủy : String, int , long , float , double , boolean , char...
-        // Kiễu dự liệu vùng nhớ (Object) : Object , Array;
+//        2 : Toán tử
 
-        String name = "Nguyễn Văn Tý";
-        char text = 'a';
-        int age = 12;
-        float height = 1.2f;
-        double weight = 35.5;
-        boolean isMale = true;
+        // Th1 a++:
+        // Gán giá trị
+        // Tăng thêm 1 giá trị
 
-        // build in function (phương thức được xây dựng sẵn)
-        // Xử lý chuỗi : "abc " + saewqe ( + chỉ dùng nhưu phép nối khi xuất hiện giá trị chuỗi)
-        System.out.println("Name : " + name);
-        System.out.println("Char : " + text);
-        System.out.println("Age : " + age);
-        System.out.println("Height : " + height);
-        System.out.println("Weight : " + weight);
-        System.out.println("isMale : " + isMale);
+        // Th2 ++a:
+        // Tăng thêm 1 giá trị
+        // Gán giá trị
 
+
+        // Độ ưu tiên
+        // ++ , --
+        // * /
+        // + -
+//        int a = 5;
+//        int b = 5;
+//
+//        int ketQua = a++ - --b + a-- - --a + b-- - --b;
+//         5 - --b + a-- - --a + b-- - --b; a = 6 , b = 5
+//         5 - 4 + a-- - --a + b-- - --b; a = 6 , b = 4
+//         5 - 4 + 6 - --a + b-- - --b; a = 5 , b = 4
+//         5 - 4 + 6 - 4 + b-- - --b; a = 4 , b = 4
+//         5 - 4 + 6 - 4 + 4 - --b; a = 4 , b = 3
+//         5 - 4 + 6 - 4 + 4 - 2; a = 4 , b = 2
+//         Ket qua 5 , a = 4 , b = 2
+//
+//        System.out.println("Kết quả " + ketQua);
+//        System.out.println("A " + a);
+//        System.out.println("B " + b);
+//
+//         Thái Ket qua: 4 , a: 4 ,b: 2
+//         Hoai Kết quả 5; a = 4 b = 1
+//         Nhan ketQua =3 ,a = 5,b= 4
+//         Truong ket qua = 3 , a = 5 , b = 4
+
+
+        int a = 1;
+        int b = 2;
+
+        int ketQua = a++ - --b + b-- - b++ + a-- - a++;
+        //1 - --b + b-- - b++ + a-- - a++; a = 2 , b = 2
+        //1 - 1 + b-- - b++ + a-- - a++; a = 2 , b = 1
+        //1 - 1 + 1 - b++ + a-- - a++; a = 2 , b = 0
+        //1 - 1 + 1 - 0 + a-- - a++; a = 2 , b = 1
+        //1 - 1 + 1 - 0 + 2 - a++; a = 1 , b = 1
+        //1 - 1 + 1 - 0 + 2 - 1; a = 2 , b = 1
+        // kq 2 , a = 2 , b1
+
+        System.out.println("Kết quả " + ketQua);
+        System.out.println("A " + a);
+        System.out.println("B " + b);
+
+        // Bổn : kết quả 1, a=2,b=1
+        // Phuong : a =3 ,b=1 ket qua = 1
+        // Quang : ketQua =2; a=2; b=1
+        // Thái : ketQua =2; a=2; b=1
     }
 }
