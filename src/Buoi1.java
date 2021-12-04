a59ca7112380d46e3a0b37c931079efb4549bc17
phamtanphat <phatdroid94@gmail.com>
2021-12-04 18:38:46 +0700

    println

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -13,6 +13,16 @@ public class Buoi1 {
         int age = 12;
         float height = 1.2f;
         double weight = 35.5;
+        boolean isMale = true;
+
+        // build in function (phương thức được xây dựng sẵn)
+        // Xử lý chuỗi : "abc " + saewqe ( + chỉ dùng nhưu phép nối khi xuất hiện giá trị chuỗi)
+        System.out.println("Name : " + name);
+        System.out.println("Char : " + text);
+        System.out.println("Age : " + age);
+        System.out.println("Height : " + height);
+        System.out.println("Weight : " + weight);
+        System.out.println("isMale : " + isMale);
 
     }
 }
