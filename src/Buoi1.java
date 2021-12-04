4fdeed9393456dafc2a0aa5e072ecefe42dc3e02
phamtanphat <phatdroid94@gmail.com>
2021-12-04 18:23:20 +0700

    Initial commit

---
+++ src/Buoi1.java
@@ -0,0 +1,18 @@
+public class Buoi1 {
+    // tổ hợp phím
+    // gợi ý code : ctrl + space
+    public static void main(String[] args) {
+
+        // 1 : Khai báo biến (variable) và kiểu dữ liệu (type)
+
+        // Kiểu dữ liệu nguyên thủy : String, int , long , float , double , boolean , char...
+        // Kiễu dự liệu vùng nhớ (Object) : Object , Array;
+
+        String name = "Nguyễn Văn Tý";
+        char text = 'a';
+        int age = 12;
+        float height = 1.2f;
+        double weight = 35.5;
+
+    }
+}
